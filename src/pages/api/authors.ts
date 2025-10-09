d8d323169df2126c25f8c44d1215ae6bf0dea1b3
Susanagc <susigc0304@gmail.com>
2025-10-09 14:01:57 -0500

    api authors

--- src/pages/api/authors.ts
+++ src/pages/api/authors.ts
@@ -5,6 +5,41 @@ import Authors from "../../database/models/authors"
 interface Author {
     authorId: number;
     name: string;
-    nationality: string,
-    birthYear: 
+    nationality: string;
+    birthYear: number;
+    isActive: boolean;
 }
+
+type GetResponse = { ok: true; data: Author[] };
+type PostResponse = { ok: true; message: string; createdId?: string };
+type PutResponse = { ok: true; message: string; updatedId?: string };
+type DeleteResponse = { ok: true; message: string; deletedId?: string };
+type ErrorResponse = { ok: true; error: string };
+
+type ResponseBody = 
+    | GetResponse
+    | PostResponse
+    | PutResponse
+    | DeleteResponse
+    | ErrorResponse
+
+    const allowed = [ 'GET', 'POST', 'PUT', 'DELETE' ];
+
+    export default async function handler(
+        req: NextApiRequest,
+        res: NextApiResponse<ResponseBody>,
+    ) {
+        
+        try {
+
+            if (!allowed.includes(req.method!)){
+                res.setHeader("Allow", allowed);
+                return res.status(405).end(`Method ${req.method} Not Allowed`)
+            }
+
+            dbConnection()
+
+            if (req.method === )
+        }
+    }
+
